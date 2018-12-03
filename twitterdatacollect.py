625cc6c86ac5f75317baaf126d5a61cf305defff
Sonali <sonali.singhal1999@gmail.com>
2018-12-03 14:20:31 +0530

    Populating Mongodb database

--- twitterdatacollect.py
+++ twitterdatacollect.py
@@ -1,55 +1,66 @@
 import tweepy
-from tweepy import OAuthHandler
+from tweepy import AppAuthHandler
 from pymongo import MongoClient
-import json
 
- 
 consumer_key = 'AtSpb7cH7fxCULkMZigy3w4LB'
 consumer_secret = 'y7KI2sJfskZZzyQ4RhImnB3dPqaxVXp1IqNp21IxR7oATH8nVA'
-access_token = '965458435033653248-ZWv7CACfWAVQEq8bTzHlFifEijjhYmO'
-access_secret = 'L7TNG2GpyVFwlCngKTdP2RQ5cn1fmw3lfBpxg8zbJlVPA'
+#access_token = '965458435033653248-ZWv7CACfWAVQEq8bTzHlFifEijjhYmO'
+#access_secret = 'L7TNG2GpyVFwlCngKTdP2RQ5cn1fmw3lfBpxg8zbJlVPA'
  
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
- 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+auth = AppAuthHandler(consumer_key, consumer_secret) #To increase the rate of queries
+#auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-def get_reply(user,tweet_id):
-	replies=[]
-	# for full_tweets in tweepy.Cursor(api.user_timeline,screen_name=user).items():
-	for tweet in tweepy.Cursor(api.search,q='to:'+user,since_id=tweet_id).items():
+if (not api): #Error handling
+    print ("Problem connecting to API")
+
+#Connect to Mongodb
+client = MongoClient('mongodb://localhost/twitterdb')  #assuming there is already a database called twitterdb
+db=client.twitterdb
+
+#Gets replies to all tweets collected
+def get_reply(user,list_tweet_ids):
+	print("Getting Reply tweets of user "+user)
+	c=0    #Counter
+	for tweet in tweepy.Cursor(api.search,q='to:'+user).items():
 		if 'in_reply_to_status_id_str' in tweet._json:
-			if (tweet._json['in_reply_to_status_id_str']==tweet_id):
-				print(tweet._json['text'])
-				replies.append(tweet._json)	
-				print(len(replies))
-				if len(replies)>100:
-					return replies			
-	return replies
+			if (tweet._json['in_reply_to_status_id_str'] in list_tweet_ids):
+				if user=='realDonaldTrump':
+					db.trump_replies.insert_one(tweet._json)
+				if user=='elonmusk':
+					db.elon_replies.insert_one(tweet._json)
+				if(c%50==0):	
+					print('Added',c)
+				c+=1
+				if c>=10000:
+					return			
+	return
 
-#Posts for Trump and Elon Musk
-user_trump=[]
-user_elon=[]
-for status in tweepy.Cursor(api.user_timeline,screen_name = 'realDonaldTrump').items(10):
-	user_trump.append(status._json)
-for status in tweepy.Cursor(api.user_timeline,screen_name = 'elonmusk').items(10):
-	user_elon.append(status._json)
+#Collect posts for Trump and Elon Musk
+for status in tweepy.Cursor(api.user_timeline,screen_name = 'realDonaldTrump').items():
+	db.trump_tweets.insert_one(status._json)
+print('Finished getting Trump tweets')
+
+
+for status in tweepy.Cursor(api.user_timeline,screen_name = 'elonmusk').items():
+	db.elon_tweets.insert_one(status._json)
+print('Finished getting Elon tweets')
 
 #Collecting reply to their tweets
-reply_trump=[]
-reply_elon=[]
-c=0
-for tweet in user_trump:
+list_trumpIDs = []
+for tweet in db.trump_tweets.find():
 	tweet_id=tweet['id_str']
-	print(tweet_id)
-	print('\n',tweet['text'])
-	reply_trump.append(get_reply('realDonaldTrump',tweet_id))
-	print('reply trump',len(reply_trump[c]))
-	c+=1
+	list_trumpIDs.append(tweet_id)
+get_reply('realDonaldTrump',list_trumpIDs)
+print('Finished getting Trump replies')
 
-for tweet in user_elon:
+list_elonIDs=[]
+for tweet in db.elon_tweets.find():
 	tweet_id=tweet['id_str']
-	reply_elon.append(get_reply('elonmusk',tweet_id))
+	list_elonIDs.append(tweet_id)
+get_reply('elonmusk',list_elonIDs)
+print('Finished getting Elon replies')
+
 
 #References
 #https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
@@ -59,3 +70,4 @@ for tweet in user_elon:
 #https://stackoverflow.com/questions/2693553/replies-to-a-particular-tweet-twitter-api
 #https://pythondata.com/collecting-storing-tweets-with-python-and-mongodb/
 #https://stellamindemography.wordpress.com/2018/05/04/mining-data-from-twitter-and-replies-to-tweets-with-tweepy/
+#https://bhaskarvk.github.io/2015/01/how-to-use-twitters-search-rest-api-most-effectively./
