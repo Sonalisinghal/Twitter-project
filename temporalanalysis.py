0a1bd1dfa2b41990617b36e62bc501c6e26c02ff
Sonali <sonali.singhal1999@gmail.com>
2018-12-05 01:41:44 +0530

    Temporal analysis test file

--- temporalanalysis.py
+++ temporalanalysis.py
@@ -3,35 +3,39 @@ import plotly.graph_objs as go
 from dateutil.parser import parse
 from load_pickle import df_trump_tweets as tt, df_trump_replies as tr, df_elon_tweets as et, df_elon_replies as er
 
-
-
-# df = web.DataReader('AAPL.US', 'quandl',
-#                     datetime(2007, 10, 1),
-#                     datetime(2009, 4, 1))
-
+print(et['created_at'])
 def get_time_occurance(l):
     date_dict={}
     for d in l:
         d=parse(d)
-        d=d.strftime('%b %d %Y')
+        d=d.strftime('%Y-%m-%d')
         if d not in date_dict:
             date_dict[d]=1
         else:
             date_dict[d]+=1
     return date_dict
 d1=get_time_occurance(tt['created_at'])
-d2=get_time_occurance(tr['created_at'])
-#print(d1)
-name2='Trump replies'
-title='Trump ANALYSER'
-#print((tt['created_at']))
+d2=get_time_occurance(et['created_at'])
 
-trace=go.Scatter(x=list(d2.keys()),y=list(d2.values()),name=name2)
+trace_high = go.Scatter(
+    x=list(d1.keys()),
+    y=list(d1.values()),
+    name = "Trump tweets",
+    line = dict(color = '#17BECF'),
+    opacity = 0.8)
 
-data = [trace]
+trace_low = go.Scatter(
+    x=list(d2.keys()),
+    y=list(d2.values()),
+    name = "Elon tweets",
+    line = dict(color = '#7F7F7F'),
+    opacity = 0.8)
+
+data = [trace_high,trace_low]
+
 layout = dict(
-    title=title,
-   xaxis=dict(
+    title='Time Series with Rangeslider',
+    xaxis=dict(
         rangeselector=dict(
             buttons=list([
                 dict(count=1,
@@ -42,64 +46,15 @@ layout = dict(
                      label='6m',
                      step='month',
                      stepmode='backward'),
-                dict(count=1,
-                    label='YTD',
-                    step='year',
-                    stepmode='todate'),
-                dict(count=1,
-                    label='1y',
-                    step='year',
-                    stepmode='backward'),
                 dict(step='all')
             ])
-       ),
+        ),
         rangeslider=dict(
             visible = True
         ),
         type='date'
-   )
+    )
 )
 
-fig = go.Figure(data=data, layout=layout)
-py.iplot(fig)
-
-
-
-
-# trace = go.Scatter(x=list(df.index),
-#                    y=list(df.High))
-
-# data = [trace]
-# layout = dict(
-#     title='Time series with range slider and selectors',
-#     xaxis=dict(
-#         rangeselector=dict(
-#             buttons=list([
-#                 dict(count=1,
-#                      label='1m',
-#                      step='month',
-#                      stepmode='backward'),
-#                 dict(count=6,
-#                      label='6m',
-#                      step='month',
-#                      stepmode='backward'),
-#                 dict(count=1,
-#                     label='YTD',
-#                     step='year',
-#                     stepmode='todate'),
-#                 dict(count=1,
-#                     label='1y',
-#                     step='year',
-#                     stepmode='backward'),
-#                 dict(step='all')
-#             ])
-#         ),
-#         rangeslider=dict(
-#             visible = True
-#         ),
-#         type='date'
-#     )
-# )
-
-# fig = dict(data=data, layout=layout)
-# py.iplot(fig)
+fig = dict(data=data, layout=layout)
+py.iplot(fig, filename = "Temporal Analysis of tweets")
